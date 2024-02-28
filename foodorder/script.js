eeea9b6128b26abc7be4862b4fa56778e0352b26
mugesh <151634675+mugesh0709@users.noreply.github.com>
2024-02-28 11:06:05 +0530

    Add files via upload

---
+++ foodorder/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+
+function Top(){
+  window.scrollTo(0,0);
+}
